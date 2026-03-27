7f13ad6e807c37a76bed71957c5d5103f2b4dc81
Abigail Ford <abigail.ford@usu.edu>
2026-03-27 18:01:06 +0000

    updated fact sales and product table

--- models/ecoessentials/eco_dim_product.sql
+++ models/ecoessentials/eco_dim_product.sql
@@ -1,12 +1,12 @@
 {{ config(
     materialized = 'table',
-    schema = 'dw_ecoessentials_ecoessentials_transactional_db'
+    schema = 'dw_ecoessentials'
     )
 }}
 
-SELECT
+select
 {{ dbt_utils.generate_surrogate_key(['product_id', 'product_name']) }} as product_key,
 product_id, 
 product_type, 
 product_name
-FROM {{ source('ecoessentials_landing', 'product') }}
+from {{ source('ecoessentials_landing', 'product') }}
